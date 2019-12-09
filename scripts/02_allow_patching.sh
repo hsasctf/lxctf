6c08a082ff0c533c493edac722e6f15e9f6cf3d1
cgo <cgo@posteo.de>
2019-12-09 21:37:47 +0100

    update bash scripts (untested)

--- scripts/02_allow_patching.sh
+++ scripts/02_allow_patching.sh
@@ -6,4 +6,5 @@
 for i in {1..127}
 do
    iptables -I FORWARD -s "10.41.$i.0/24" -d "10.40.$i.1" -j ACCEPT -m comment --comment "time for defense"
+   iptables -I FORWARD -s "10.42.$i.0/24" -d "10.40.$i.1" -j ACCEPT -m comment --comment "time for defense"
 done
