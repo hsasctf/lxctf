eee229d90645874ea372ae3fa3bcdcf3df3601c4
cgo <cgo@posteo.de>
2020-02-03 19:02:25 +0100

    remove unused code

--- ctfdbapi/dashboard_worker.py
+++ ctfdbapi/dashboard_worker.py
@@ -49,49 +49,6 @@ class RedisUpdater(object):
         for service_data in services_data:
             self.services_names[service_data["service_id"]] = service_data["service_name"]
 
-    def ctf_services(self):
-
-        url = '/'.join([self.api_url, "getlatestflagids"])
-        r = requests.get(url, params=self.params)
-
-        # print(r.json()["flag_ids"])
-
-        # {'113': {'114': '545'}, '114': {'114': '546'}, '115': {'114': '543'}, '116': {'114': '544'}}
-        #  team         srv     flag_id
-
-        flag_ids = r.json()["flag_ids"]
-
-        if len(flag_ids) == 0:
-            logger.warning("No flag IDs found")
-
-        if any(len(x) == 0 for x in flag_ids.values()):
-            logger.warning("For at least one team the mapping from service to flag_id is empty: {}. This means the scorebot is not running or the game is not started yet or the scorebot has found services that are offline.".format(flag_ids))
-
-        services = {}
-        services_info = self.gameinfo['services']
-        for service_info in services_info:
-            service_id = service_info['service_id']
-
-            if service_id not in services:
-                services[service_id] = {}
-
-            services[service_id]['description'] = service_info['description']
-            services[service_id]['port'] = service_info['port']
-            services[service_id]['name'] = service_info['service_name']
-            services[service_id]['flag_id'] = \
-                {
-                    'description': service_info['flag_id_description'],
-                    'flag_ids': [
-                        {
-                            'team_id': team_subnet_id,
-                            'flag_id': flag_ids[team_subnet_id][str(service_id)],
-                        } for team_subnet_id in
-                        dict(list(filter(lambda kv: str(service_id) in kv[1], flag_ids.items())))
-                    ]
-                }
-
-        self.store_redis('ctf_services', json.dumps(services))
-
     def ctf_services2(self):
 
         url = '/'.join([self.api_url, "getlatestflagids_multi"])
@@ -108,7 +65,9 @@ class RedisUpdater(object):
             logger.warning("No flag IDs found")
 
         if any(len(x) == 0 for x in flag_ids.values()):
-            logger.warning("For at least one team the mapping from service to flag_id is empty: {}. This means the scorebot is not running or the game is not started yet or the scorebot has found services that are offline.".format(flag_ids))
+            logger.warning(
+                "For at least one team the mapping from service to flag_id is empty: {}. This means the scorebot is not running or the game is not started yet or the scorebot has found services that are offline.".format(
+                    flag_ids))
 
         services = {}
         services_info = self.gameinfo['services']
